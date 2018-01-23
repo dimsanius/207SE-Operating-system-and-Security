e21bf1212a278daa114582f6b45d6952d701af02
Dmitrij Santarovic <santarod@uni.coventry.ac.uk>
2018-01-23 14:19:37 +0000

    vocabulary defined

--- Portfolio 1, ex b1/Portfolio 1, ex b1/main.cpp
+++ Portfolio 1, ex b1/Portfolio 1, ex b1/main.cpp
@@ -6,17 +6,32 @@ using namespace std;
 
 int main() 
 { 
+	cout << "Enter string to analyze: ";
 	//set up input string 
-	string input="bot pick ball"; 
+	string input = "bot pick ball"; 
+
 	//initalise input stream string
-	stringstream currentstring(input); 
-	int count=-1; 
-	string word[10];
+	stringstream currentstring(input);
+	
+	string word[10] = {};
+
+	//predefined vocabulary
+	string PersonWords[] = { "Nao", "Shila" };
+	string BehaviourWords[] = {"recognise", "detect", "avoid", "replace"};
+	string PositionWords[] = { "above", "below", "left", "right" };
+	string PronounWords[] = { "I", "you", "they" };
+	string ObjectWords[] = { "car", "person", "dog", "diamond" };
+	string TextureWords[] = { "smooth", "dimpled", "rough" };
+	bool followsTheRule = true;
 	//Repeatedly put words in string array 
-	while (currentstring.good())
+	for (int i = 0; currentstring.good() && followsTheRule; i++)
 	{
-		count=count+1; 
-		currentstring >> word[count]; 
-	} 
+		currentstring >> word[i];
+		cout << word[i] << endl;
+	}
+	cout << currentstring.seekp();
+	//if(word[0] == PersonWords[i])
+	//for(int i = 0; i < word->length; i++)
+	system("PAUSE");
 	return 0; 
 }
